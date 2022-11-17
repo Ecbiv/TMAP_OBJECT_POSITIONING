5284c395554dc7d26c87ea7e30354b6473258262
Ecbiv <edward@keyandcorner.com>
2022-11-17 12:59:08 -0700

    Importing explicits from keras.layers

--- CNNRegressAndSoftmax.py
+++ CNNRegressAndSoftmax.py
@@ -1,33 +1,36 @@
 import tensorflow as tf
+from tensorflow import keras
+from keras.layers import Dense, Input, Flatten, Conv2D, MaxPool2D, BatchNormalization
 
 def conv_block(input):
-    output = tf.keras.layers.Conv2D(32, 3, padding = 'same', activation = 'relu')(input)
-    output = tf.keras.layers.BatchNormalization()(output)
-    output = tf.keras.layers.MaxPool2D(2)(output)
+    output = Conv2D(32, 3, padding = 'same', activation = 'relu')(input)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(2)(output)
             
-    output = tf.keras.layers.Conv2D(64, 3, padding = 'same', activation = 'relu')(output)
-    output = tf.keras.layers.BatchNormalization()(output)
-    output = tf.keras.layers.MaxPool2D(2)(output)
+    output = Conv2D(64, 3, padding = 'same', activation = 'relu')(output)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(2)(output)
             
-    output = tf.keras.layers.Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
-    output = tf.keras.layers.BatchNormalization()(output)
-    output = tf.keras.layers.MaxPool2D(2)(output)
+    output = Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(2)(output)
                 
-    output = tf.keras.layers.Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
-    output = tf.keras.layers.BatchNormalization()(output)
-    output = tf.keras.layers.MaxPool2D(2)(output)
+    output = Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(2)(output)
     return output
 
 def regression_block(input):
-    output = tf.keras.layers.Flatten()(input)
-    output = tf.keras.layers.Dense(1024, activation = 'relu')(output)
-    output = tf.keras.layers.Dense(512, activation = 'relu')(output)
-    output = tf.keras.layers.Dense(2, name = 'boundary box')(output)
+    output = Flatten()(input)
+    output = Dense(1024, activation = 'relu')(output)
+    output = Dense(512, activation = 'relu')(output)
+    output = Dense(2, name = 'boundary box')(output)
     return output
 
 def classification_block(input):
-    output = tf.keras.layers.Flatten()(input)
-    output = tf.keras.layers.Dense(1024, activation = 'relu')(output)
-    output = tf.keras.layers.Dense(512, activation = 'relu')(output)
-    output = tf.keras.layers.Dense(6, activation = 'softmax', name = 'class')(output)
+    output = Flatten()(input)
+    output = Dense(1024, activation = 'relu')(output)
+    output = Dense(512, activation = 'relu')(output)
+    output = Dense(6, activation = 'softmax', name = 'class')(output)
     return output
+
