956b42910d6d531942d40ac343fe3d70f41158d5
Ecbiv <edward@keyandcorner.com>
2022-11-17 09:56:51 -0700

    Begin rough draft CNN

--- CNN-regress-and-softmax.py
+++ CNN-regress-and-softmax.py
@@ -1,2 +1,20 @@
 import tensorflow as tf
 
+#batch size = 32
+def conv_block(input):
+    output = tf.keras.layers.Conv2D(32, 3, padding = 'same', activation = 'relu')(input)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.MaxPool2D(2)(output)
+        
+    output = tf.keras.layers.Conv2D(64, 3, padding = 'same', activation = 'relu')(output)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.MaxPool2D(2)(output)
+        
+    output = tf.keras.layers.Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.MaxPool2D(2)(output)
+            
+    output = tf.keras.layers.Conv2D(128, 6, padding = 'valid', activation = 'relu')(output)
+    output = tf.keras.layers.BatchNormalization()(output)
+    output = tf.keras.layers.MaxPool2D(2)(output)
+    return output
