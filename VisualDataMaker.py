dca862fb637ff4a9b8a36358aefd790ef8be3f32
Ecbiv <edward@keyandcorner.com>
2022-11-17 09:00:30 -0700

    changed ground truth bounding box color to blue

--- VisualDataMaker.py
+++ VisualDataMaker.py
@@ -53,7 +53,7 @@ def plot_bounding_box(image_set, pred_coords = None):
         img_array, classification, coordinates, size = image[0], image[1], image[2], image[3]
         img_literal = Image.fromarray(img_array)
         draw = ImageDraw.Draw(img_literal)
-        draw.rectangle((coordinates[0], coordinates[1], coordinates[0] + size, coordinates[1] + size), outline = 'green', width = 2)
+        draw.rectangle((coordinates[0], coordinates[1], coordinates[0] + size, coordinates[1] + size), outline = 'blue', width = 2)
         if pred_coords:
             draw.rectangle((pred_coords[0], pred_coords[1], pred_coords[0] + size, pred_coords[1] + size), outline = 'red', width = 2)
         bounded_image_set.append([np.array(img_literal), classification, coordinates, size])
