df12610359c84ceb3b16027dc5a285096c775c68
Ecbiv <edward@keyandcorner.com>
2022-11-18 22:19:40 -0700

    Loading total model from .py file into .ipynb

--- CNNRegressionAndSoftmax.py
+++ CNNRegressionAndSoftmax.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from keras import Sequential
-from keras.layers import Dense, Input, Flatten, Conv2D, MaxPool2D, BatchNormalization, CategoryEncoding
+from keras.layers import Dense, Input, Flatten, Conv2D, MaxPool2D, BatchNormalization
 
 def OHE_encoder(output):
     OHE = [0]*len(output)
@@ -17,6 +17,12 @@ def convolutional_block(input):
     output = BatchNormalization()(output)
     output = MaxPool2D(pool_size=(2,2))(output)
     output = Conv2D(filters = 32, kernel_size = (3,3), padding = 'same', activation = 'relu')(output)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(pool_size=(2,2))(output)
+    output = Conv2D(filters = 64, kernel_size = (3,3), padding = 'same', activation = 'relu')(output)
+    output = BatchNormalization()(output)
+    output = MaxPool2D(pool_size=(2,2))(output)
+    output = Conv2D(filters = 64, kernel_size = (3,3), padding = 'same', activation = 'relu')(output)
     output = BatchNormalization()(output)
     output = MaxPool2D(pool_size=(2,2))(output)
     return output
@@ -34,3 +40,13 @@ def classification_block_forward(input):
     output = Dense(512, activation = 'relu')(output)
     output = Dense(10, activation = 'softmax', name = 'class')(output)
     return output
+
+
+def getUntrainedModel(input_shape = (224, 224, 3)):
+    input = Input(input_shape)
+    x = convolutional_block(input)
+    box_output = regression_block_forward(x)
+    class_output = classification_block_forward(x)
+    model = keras.Model(inputs=input, outputs = [class_output, box_output])
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss={'class': 'categorical_crossentropy', 'boundary_box': 'mse'}, metrics={'class': 'accuracy', 'boundary_box': 'mse'})
+    return model
